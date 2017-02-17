3101c6c7cdbf06042268bdc1e2c4bf755313a4fe
Douglas Dunn <douglas.dunn@outlook.com>
2017-02-16 21:54:36 -0800

    made the website responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -19,5 +19,22 @@ $(document).ready(function() {
         });
     });
 
+    $(".js--section-services").waypoint(function(direction) {
+        if (direction === "down") {
+            $("nav").addClass("sticky");
+        } else {
+            $("nav").removeClass("sticky");
+        }
+    }, {
+        //  offset: '635px'
+        offset: '605px'
+        // offset: '500px'
+    });
+
+    // var waypoints = $('#handler-first').waypoint(function(direction) {
+    //     notify(this.element.id + ' hit 25% from top of window')
+    // }, {
+    //     offset: '25%'
+    // })
 
 });
